47f14518a191e0033be6e99bf67fa93c5fc13465
Gurdeep Singh <155575643+Gurdeepsingh-10@users.noreply.github.com>
2025-08-06 18:02:13 +0530

    initial commit

---
+++ backend/data/combine_data.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import os
+
+data_dir = os.path.join("data","raw")
+cities = ["Delhi", "Mumbai", "Pune"]
+all_dataframes = []
+
+for city in cities:
+    file_path = os.path.join(data_dir, f"{city}.csv")
+    print(f"Reading data from: {file_path}")
+    df = pd.read_csv(file_path)
+    df['City'] = city  
+    all_dataframes.append(df)
+
+combined_df = pd.concat(all_dataframes, ignore_index=True)
+
+print("Combined DataFrame:")
+print(combined_df.head())
+
+output_path = os.path.join("data", "processed", "combined_data.csv")
+os.makedirs(os.path.dirname(output_path), exist_ok=True)
+combined_df.to_csv(output_path, index=False)
+
+
+print(f"\nCombined data saved to: {output_path}")
